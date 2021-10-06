338c78e4d6bd78d7b774f06c6d6c5af136fd4c39
Sxtanna <ranald.taylor@gmail.com>
2021-10-06 13:53:53 -0400

    docs: added documentation for JsonKey interface

--- src/main/java/com/sxtanna/mc/json/JsonKey.java
+++ src/main/java/com/sxtanna/mc/json/JsonKey.java
@@ -12,24 +12,55 @@ import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.function.Function;
 
+/**
+ * Represents the information necessary to encode and decode a {@link JsonElement} to/from {@code T} at a certain path
+ *
+ * @param <T> The type this key is encoding and decoding
+ */
 public interface JsonKey<T>
 {
 
+    /**
+     * @return The path this key points to
+     */
     @NotNull Pxth pxth();
 
+    /**
+     * @return The type of the value at this key
+     */
     @NotNull Type type();
 
 
+    /**
+     * Represents a {@link JsonKey} that directly encodes and decodes its value
+     *
+     * @param <T> The type this key is encoding and decoding
+     */
     interface Direct<T> extends JsonKey<T>
     {
 
+        /**
+         * Encodes the provided {@link T} into a {@link JsonElement}
+         *
+         * @param data The data to encode
+         * @return The encoded {@link JsonElement}, or {@link com.google.gson.JsonNull#INSTANCE} if null
+         */
         @NotNull JsonElement into(@Nullable final T data);
 
+        /**
+         * Decodes from the provided {@link JsonElement} to {@link T}
+         *
+         * @param json The json to decode
+         * @return The decoded {@link T}, or {@code null}
+         */
         @Nullable T from(@NotNull final JsonElement json);
 
     }
 
 
+    /**
+     * @return The new {@link JsonKey} with the provided {@link Pxth} and {@link Class}
+     */
     @Contract(value = "_, _ -> new", pure = true)
     static @NotNull <T> JsonKey<T> of(@NotNull final Pxth pxth,
                                       @NotNull final Class<T> type)
@@ -50,6 +81,9 @@ public interface JsonKey<T>
         };
     }
 
+    /**
+     * @return The new {@link JsonKey} with the provided {@link Pxth} and type resolved from the {@link TypeToken}
+     */
     @Contract(value = "_, _ -> new", pure = true)
     static @NotNull <T> JsonKey<T> of(@NotNull final Pxth pxth,
                                       @NotNull final TypeToken<T> type)
@@ -71,6 +105,9 @@ public interface JsonKey<T>
     }
 
 
+    /**
+     * @return The new {@link JsonKey.Direct} with the provided {@link Pxth} and {@link Class} that uses the provided into and from functions
+     */
     @Contract(value = "_, _, _, _ -> new", pure = true)
     static @NotNull <T> JsonKey<T> of(@NotNull final Pxth pxth,
                                       @NotNull final Class<T> type,
@@ -107,6 +144,9 @@ public interface JsonKey<T>
         };
     }
 
+    /**
+     * @return The new {@link JsonKey.Direct} with the provided {@link Pxth} and type resolved from the {@link TypeToken} that uses the provided into and from functions
+     */
     @Contract(value = "_, _, _, _ -> new", pure = true)
     static @NotNull <T> JsonKey<T> of(@NotNull final Pxth pxth,
                                       @NotNull final TypeToken<T> type,
